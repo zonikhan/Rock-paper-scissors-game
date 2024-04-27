fde4421bad9ba08bb8ba07b593d9df2bedcac23c
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-04-27 19:56:00 +0500

    Rock-paper-scissors-game

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// IF ELSE STATEMENT USING ROCK PAPER SCISSORS GAME
+let player1 = "Rock";
+let player2 = "paper";
+if (player1 === player2) {
+    console.log("its a tie!");
+}
+else if ((player1 === "Rock" && player2 === "Scissors") ||
+    (player1 === "Scissors" && player2 === "Paper") ||
+    (player1 === "Paper" && player2 === "Rock")) {
+    console.log("player 1 wins this match");
+}
+else {
+    console.log("player 2 wins this math sucessfully");
+}
+export {};
